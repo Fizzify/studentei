bb786f0fc2d1b4361278026ca1ace7f9bab1c8ee
Fizzify <fizzyplayzoriginal5@gmail.com>
2023-07-15 19:36:25 +0800

    Add tables and filtering.

--- src/server/api/routers/student.ts
+++ src/server/api/routers/student.ts
@@ -1,7 +1,22 @@
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const studentRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.student.findMany();
-  }),
+  getAll: publicProcedure
+    .input(z.string().optional())
+    .query(async ({ ctx, input }) => {
+      if (input && typeof input == "string") {
+        const students = await ctx.prisma.student.findMany();
+
+        const filteredStudents = students.filter((student) =>
+          Object.values(student).some((value) =>
+            String(value).toLowerCase().includes(input.toLowerCase())
+          )
+        );
+
+        return filteredStudents;
+      } else {
+        return await ctx.prisma.student.findMany();
+      }
+    }),
 });
